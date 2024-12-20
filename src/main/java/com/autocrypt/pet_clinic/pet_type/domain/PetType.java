dfc04c8d1e951c017018af5fdbe1af06783a4dee
정준우 <80523328+bviz6542@users.noreply.github.com>
2024-12-20 14:11:13 +0900

    🐛 pet type의 MappedCollection 제거

--- src/main/java/com/autocrypt/pet_clinic/pet_type/domain/PetType.java
+++ src/main/java/com/autocrypt/pet_clinic/pet_type/domain/PetType.java
@@ -1,14 +1,9 @@
 package com.autocrypt.pet_clinic.pet_type.domain;
 
-import com.autocrypt.pet_clinic.owner.domain.Pet;
 import lombok.Getter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Column;
-import org.springframework.data.relational.core.mapping.MappedCollection;
 import org.springframework.data.relational.core.mapping.Table;
-
-import java.util.HashSet;
-import java.util.Set;
 
 @Getter
 @Table("pet_type")
@@ -19,7 +14,4 @@ public class PetType {
     private Long id;
 
     private String name;
-
-    @MappedCollection(idColumn = "pet_type_id")
-    private Set<Pet> pets = new HashSet<>();
 }
