179075ea93117fb181022d1962faf6f5afd06679
정준우 <80523328+bviz6542@users.noreply.github.com>
2024-12-20 22:32:40 +0900

    💄 홈 화면 구현, 홈 컨트롤러 구현

---
+++ src/main/java/com/autocrypt/pet_clinic/web/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.autocrypt.pet_clinic.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String homePage() {
+        return "home";
+    }
+}
