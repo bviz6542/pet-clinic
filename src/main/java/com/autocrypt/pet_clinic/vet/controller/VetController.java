e8111d2783cb7a40c548b3b817ab50b9e95a07fe
정준우 <80523328+bviz6542@users.noreply.github.com>
2024-12-20 22:55:01 +0900

    💄 수의사 화면 구현

--- src/main/java/com/autocrypt/pet_clinic/vet/controller/VetController.java
+++ src/main/java/com/autocrypt/pet_clinic/vet/controller/VetController.java
@@ -5,16 +5,25 @@ import com.autocrypt.pet_clinic.vet.service.VetService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/vets")
 public class VetController {
 
     private final VetService vetService;
 
-    @GetMapping( "/vets")
+    @GetMapping( "/json")
     public ResponseEntity<VetListDto> showResourcesVetList() {
         return ResponseEntity.ok(vetService.getAllVets());
+    }
+
+    @GetMapping
+    public String vetListPage(Model model) {
+        model.addAttribute("vetList", vetService.getAllVets());
+        return "vets/vetList";
     }
 }
